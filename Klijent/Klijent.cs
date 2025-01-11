9cbfe7245631af217ffd3c433cd1d6ac7a5dc533
Majaaa3 <majacimbaljevic3@gmail.com>
2025-01-11 16:39:10 +0100

    Izmjene

--- Klijent/Klijent.cs
+++ Klijent/Klijent.cs
@@ -8,8 +8,28 @@ namespace Klijent
     {
         static void Main(string[] args)
         {
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint serverEP=new IPEndPoint(IPAddress.Any, 50001);
+            EndPoint posiljaocEP = new IPEndPoint(IPAddress.Any, 0);
 
-            
+            Console.WriteLine("Izvrsite prijavu: ");
+            string prijava="PRIJAVA: "+Console.ReadLine();
+            byte[] binarnaPrijava=Encoding.UTF8.GetBytes(prijava);
+
+            int brojBajta = clientSocket.SendTo(binarnaPrijava, 0, binarnaPrijava.Length, SocketFlags.None, serverEP);
+
+            clientSocket.Connect(serverEP);
+
+            byte[] buffer = new byte[1024];
+            string odgovor;
+
+            int brBajta = clientSocket.Receive(buffer);
+            string serverMessage = Encoding.UTF8.GetString(buffer, 0, brBajta);
+            Console.WriteLine(serverMessage);
+
+            Console.WriteLine();
+            string spreman = Console.ReadLine();
+            clientSocket.Send(Encoding.UTF8.GetBytes(spreman));
         }
     }
 }
